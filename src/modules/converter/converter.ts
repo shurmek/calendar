2844d29e9a5bd3b052ac17b27c14571e59fa49a8
Eliseev Alexandr <aalekeliseev@sberbank.ru>
2022-04-19 02:08:39 +0300

    added leap days offset

--- src/modules/converter/converter.ts
+++ src/modules/converter/converter.ts
@@ -15,14 +15,51 @@ export class Converter implements ConverterInterface {
   }
 
   public valueToMilliseconds(value: number, unit: DateTimeUnits): number {
-    return value * unit;
+    const result = value * unit;
+
+    if (unit === DateTimeUnits.YEAR) {
+      return result + this.getLeapYearOffset(value);
+    }
+
+    return result;
   }
 
   public millisecondsToValue(milliseconds: number, unit: DateTimeUnits): number {
+    if (unit === DateTimeUnits.YEAR) {
+      milliseconds += this.getLeapYearOffset(milliseconds);
+    }
     return Math.floor(milliseconds / unit);
   }
 
   public getRestMilliseconds(milliseconds: number, unit: DateTimeUnits): number {
-    return milliseconds % unit;
+    const result = milliseconds % unit;
+
+    if (unit === DateTimeUnits.YEAR) {
+      return result - this.getLeapYearOffset(milliseconds);
+    }
+
+    return result;
+  }
+
+  private getLeapYearOffset(milliseconds: number): number {
+    const year = Math.floor(milliseconds / DateTimeUnits.YEAR);
+    let result: number = 0;
+
+    for (let i = 0; i <= year; i++) {
+      if (this.checkLeapYear(i)) {
+        result++;
+      }
+    }
+
+    return this.valueToMilliseconds(result, DateTimeUnits.DAY);
+  }
+
+  private checkLeapYear(year: number): boolean {
+    if (year % 4 === 0) {
+      if (year % 400 === 0) return true;
+      if (year % 100 === 0) return false;
+      return true;
+    }
+    return false;
   }
 }
